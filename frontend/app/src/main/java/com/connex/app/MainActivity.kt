1683ce45ea6071c28e2d293f920ffc4296bf056f
Kaede <kaede982006@gmail.com>
2026-01-30 23:57:36 +0900

    Use navigation root in main activity

--- frontend/app/src/main/java/com/connex/app/MainActivity.kt
+++ frontend/app/src/main/java/com/connex/app/MainActivity.kt
@@ -6,9 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.connex.app.ui.navigation.ConneXRoot
 import com.connex.app.ui.theme.ConneXTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,8 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // TODO: Navigation Graph
-                    Text("ConneX")
+                    ConneXRoot()
                 }
             }
         }
